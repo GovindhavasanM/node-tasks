f8b2ee099e7a69c4aab0aab1520cb92e00518c47
GovindhavasanM <santhoshmanigovi@gmail.com>
2024-12-11 10:51:29 +0530

    updated.env

--- nodetask1/config/dbConfig.js
+++ nodetask1/config/dbConfig.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://MSanthosh:DlZ6I7SPotpFZ9Tr@cluster0.se47z.mongodb.net/RecipeManager");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected");
   } catch (error) {
     console.log("Database connection failed");
